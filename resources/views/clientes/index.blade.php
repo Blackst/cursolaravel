bef633b13115ac22587b899542d98521f0d9e867
Thiago Mendes <tm.thiagom86@gmail.com>
2019-09-27 12:08:16 -0300

    Criado index para criar os ids para gerenciar os usarios na sessao

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -9,7 +9,7 @@
             {{ $c['nome'] }}
             <a href="{{route ('clientes.edit', $c['id'])}}">Editar</a> | 
             <a href="{{route ('clientes.show', $c['id'])}}">info</a> | 
-            <form action="{{ route('clientes.update', $c['id'])}}" method="POST">
+            <form action="{{ route('clientes.destroy', $c['id'])}}" method="POST">
                 @csrf
                 @method('DELETE')
                 <input type="submit" value="Apagar">
